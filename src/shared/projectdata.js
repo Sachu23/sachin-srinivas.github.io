493e42de8b33f6008cff97f78dfc55c35412eaa4
Sachin Srinivas <bblegend03@gmail.com>
2024-03-27 18:03:52 -0500

    Project_updates
    Matching the URL

--- src/shared/projectdata.js
+++ src/shared/projectdata.js
@@ -1,19 +1,21 @@
+const base_url = 'https://sachu23.github.io/sachin-srinivas.github.io'
+
 export const PROJECTS =
     [
         {
             id: 0,
             name:'Chicago Bikers Real-Time Navigation System',
-            image: '/images/bike.png',
+            image: base_url + '/images/bike.png',
             description:'Developed a mobile application using the React Native framework and utilized React and Express JavaScript for the backend.',
             link : "https://github.com/Sachu23/Flutter-Bike_Navigation",
             feature:true,
-            techstack: "Flutter, ReactJS, ExpressJS, NodeJS, MongoDB, REST API",
+            techstack: "Flutter, React-Native, ReactJS, ExpressJS, NodeJS, MongoDB, REST API",
             renderfirst: false
         },
         {
             id: 1,
             name:'MS - GSP (Minimum Support - Generalized Sequential Pattern) Mining Algorithm',
-            image: '/images/face.jpg',
+            image: base_url + '/images/face.jpg',
             description:'GSP mining can be used to analyze social media data, such as posts and comments, and identify patterns in the data that indicate trends, sentiment, or other insights.',
             link: 'https://github.com/Sachu23/DMTM---MS-GSP-Algo-Implementation',
             feature:false,
@@ -23,7 +25,7 @@ export const PROJECTS =
         {
             id: 2,
             name:'Twitter Sentiment Analysis',
-            image: '/images/twitter.png',
+            image: base_url + '/images/twitter.png',
             description:'Built a real-time Twitter sentiment analysis app using Twitter API, Apache Kafka for data streaming, NLP techniques, and CNN for sentiment classification. Visualized results with Heat Maps and stored data in MongoDB',
             link: 'https://github.com/Sachu23/Twitter-Setiment-Analysis',
             feature:true,
@@ -33,27 +35,27 @@ export const PROJECTS =
         {
          id: 3,
             name:'Divy-Van-Assistant',
-            image: '/images/ronaldo.jpg',
+            image: base_url + '/images/ronaldo.jpg',
             description:'Software to help maintance staff to keep track of Divvy Bike inventory.',
             link: 'https://github.com/Sachu23/Divy-Van-Assistant',
             feature:false,
-            techstack: "Jupyter-Notebook, Keras",
+            techstack: "Java, JavaFX, Swing, MongoDB",
             renderfirst: true
         },
         {
             id: 4,
             name:'Cloud Orchestrator',
-            image: '/images/cloud.png',
+            image: base_url + '/images/cloud.png',
             description:'Designed and developed a Cloud Orchestrator for Docker-based REST API deployment, incorporating load balancing, fault tolerance, and auto-scaling.',
             link: 'https://github.com/Sachu23/Cloud-Orchestrator',
             feature:true,
-            techstack: "Python, OpenCV, Keras",
+            techstack: "ExpressJS, AWS EC2 instance, Docker-Container, ReactJS, NodeJS, MongoDB, Apache Kafka, Zookeeper",
             renderfirst:false
         },
         {
             id: 5,
             name:'Stock Market Prediction',
-            image: '/images/stock.jpg',
+            image: base_url+ '/images/stock.jpg',
             description:'A simple stock market prediction using logistic regression',
             link: '',
             feature:false,
